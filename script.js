745ac1435b4c9e74c96d0ee1fa5ee7b644bcb19e
MuhammadSayyidHabibie <habibiemuhammadsayyid@gmail.com>
2024-11-09 21:47:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLink = document.querySelectorAll('header navbar'); 
+
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = set.offsetheight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLink.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            })
+        }
+    })
+}
+
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
